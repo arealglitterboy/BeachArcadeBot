1f76572b6b2b7fe826cd8fc4faebced6d0cfc81e
Ben Brown <benwbrown1029@gmail.com>
2021-04-23 14:24:16 +0100

    Merged Blake's work

--- Sprint5/src/BeachArcade.java
+++ Sprint5/src/BeachArcade.java
@@ -4,6 +4,204 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public class BeachArcade implements Bot {
+    private BoardAPI board;
+	private PlayerAPI player;
+
+    private final GameMap map;
+
+	protected static int selectedTerritory; // * Holds the ID of the target of an attack
+	// * initialisation: place your troops inside the cluster, place neutrals away from you
+
+    // * 1. Select main territory (or territories)
+	// * 2. Place troops 3 at a time
+	// * 3. Battle from there, priority on inside continent
+	// * 4. Fortify walls
+
+	// ? When you take over the whole continent, build up walls, then try and take over next continent
+
+	// ? What about a constant value ATTACK_LIMIT, would be a value representing a ratio
+
+    BeachArcade(BoardAPI inBoard, PlayerAPI inPlayer) {
+        board = inBoard;
+        player = inPlayer;
+
+		map = new Map(player.getId());
+
+        map.startTurn(board);
+    }
+
+    /**
+     * <strong>getName</strong> — Gets the name of the bot.
+     *
+     * @return String, Bot's name
+     */
+    public String getName() {
+        return ("SPF 75"); // ? What about throwing debug info in here, so whenever the bot's name is used, we get debug stuff
+        /*
+         *debug will include:
+         *	how many countries in total the bot partialSetMatchs/percentage of the board partialSetMatchled
+         *	how many connecting countries the bot partialSetMatchs
+         *	standard deviation of the troops on each territory
+         *	how many cards are in the bot's hand/whether or not the bot can turn in its cards(to see if it is intentionally not turning them in)
+         * 	how many whole continents the bot owns
+         */
+    }
+
+    private String getCommand(Turn turn) {
+    	map.startTurn(board);
+
+    	return map.stream().filter(turn::canUse).findFirst().map(turn::getCommand).orElse("skip");
+	}
+
+	/**
+	 * <p><strong>getPlacement</strong> — For placing of territories in <em>initialisation stage</em>.</p>
+	 * <p>Chooses a territory from one of the neutrals to place reinforcements on sending it to the board reinforcement (from the neutral player's reserves) on the selected territory.</p>
+	 *
+	 * @param forPlayer The index of the player whose territories we are choosing from.
+	 * @return String, the name of the territory
+	 */
+	public String getPlacement(int forPlayer) {
+		System.out.println("RE-IMPLEMENT THIS");
+		return getRandomName();
+	}
+
+	/* Utility Methods */
+	public static String getRandomName() {
+		return GameData.COUNTRY_NAMES[(int) (Math.random() * GameData.NUM_COUNTRIES)].replaceAll("\\s", "");
+	}
+
+    /**
+     * <p><strong>getReinforcement</strong> — For <em>reinforcing</em> a territory with reserves.</p>
+     * <p>Gets the name of a territory (that belongs to the bot) to place a number of reinforcements (that is valid) onto.</p>
+     *
+     * @return String, command in the form "Territory Name" "Number of Reinforcements".
+     */
+    public String getReinforcement() {
+		return getCommand(Reinforcement.turn);
+    }
+
+    /**
+     * <p><strong>getCardExchange</strong> — Gets cards from the bot's hand to exchange for reserves.</p>
+     * <p>Returns a valid card exchange command; 3 cards to exchange (just first letter, ie. wia), or "<em>skip</em>" if less than 5 cards.</p>
+     *
+     * @return String, command using letters to represent the cards
+     */
+    public String getCardExchange() { // First Iteration Complete
+        // Later it might be possible to strategise what hands are optimal, and if you have 2 different ways to turn in cards, find the best one
+        int[][] validSets = new int[60][3];
+        int[] set = new int[Deck.SET_SIZE];
+        int r = 0;
+        PriorityQueue<Decision> decisions = new PriorityQueue<>();
+
+        for (int i = 0; (i < Deck.NUM_SETS); i++) {
+            System.arraycopy(Deck.SETS[i], 0, set, 0, Deck.SET_SIZE);
+            int count = 0;
+            ArrayList<Card> copyCards = new ArrayList<Card>(player.getCards()); //Takes the cards in the bot's hand and makes a copy to find possible matches with
+            boolean partialSetMatch; // PartialSetMatch is made to cut the loop when a match is found
+            for (int j : set) {
+                partialSetMatch = false;
+                for (int m = 0; (m < copyCards.size()) && !partialSetMatch; m++) {
+                    if (j == copyCards.get(m).getInsigniaId()) {
+                        copyCards.remove(m);
+                        count++;
+                        partialSetMatch = true;
+                    }
+                }
+                if (count == Deck.SET_SIZE) {
+                    System.arraycopy(set, 0, validSets[r], 0, set.length);
+                    r++;
+                    count = 0;
+                }
+            }
+        }
+        for (int i = 0; i < r; i++) { // Takes the int values of the insignias and turns them into usable answers for the bot
+            StringBuilder x = new StringBuilder();
+            System.out.println("-----------------------");
+            for (int j = 0; j < Deck.SET_SIZE; j++) {
+                switch (validSets[i][j]) {
+                    case 0:
+                        x.append("i");
+                        break;
+                    case 1:
+                        x.append("c");
+                        break;
+                    case 2:
+                        x.append("a");
+                        break;
+                    case 3:
+                        x.append("w");
+                        break;
+                    default:
+                    	System.out.println("Error you messed up");
+                }
+            }
+            int weight = 0;
+            for (int j = 0; j < Deck.SET_SIZE; j++) {
+                if (validSets[i][j] == 0 || validSets[i][j] == 1 || validSets[i][j] == 2)// Weights a set so that the wild cards are saved for later use  if possible
+                    weight += 2;
+                else
+                    weight += 1;
+
+            }
+            String possibleDecision = x.toString();
+            decisions.add(new Decision(weight, possibleDecision));
+        }
+        if (decisions.isEmpty()) {
+        	throw new IllegalStateException("No valid set of cards");
+		}
+		return decisions.poll().command;
+    }
+
+    /**
+     * <p><strong>getBattle</strong> — For getting an attack command.</p>
+     * <p>Used in bot's <em>attack stage</em>. Returns a String containing the country to attack from, the country to attack and the number of units to use, or "skip" to skip attack phase.</p>
+     *
+     * @return String representing attack command ("from", "to", "units")
+     */
+    public String getBattle() {
+    	return getCommand(Battle.turn);
+    }
+
+    /**
+     * <p><strong>getDefence</strong> — For getting a defend command.</p>
+     * <p>Used when other player is in the <em>attack stage</em>. Returns the number of units to defend a territory with.</p>
+     *
+     * @param countryId int, ID of territory to defend.
+     * @return String, number of units
+     */
+    public String getDefence(int countryId) { // Always defend with the most units you can to give the best chance of winning
+        String command = "";
+        if (board.getNumUnits(countryId) >= 2)
+            command += 2;
+        else
+            command += 1;
+        return (command);
+    }
+
+    /**
+     * <p><strong>getMoveIn</strong> — For sending troops after winning battle.</p>
+     * <p>Used when you have won a battle and claimed a territory. Sends a number of troops to the newly conquered territory, leaving both territories with at least one troop. </p>
+     *
+     * @param attackCountryId int, ID of your attacking territory
+     * @return String, number of units
+     */
+    public String getMoveIn(int attackCountryId) {
+//        return getCommand(MoveIn.turn);
+		// > Logic surrounding the value in `selectedTerritory` and the value in attackCountryId
+		Territory attacking = map.getTerritory(attackCountryId);
+		return "";
+    }
+
+	/**
+	 * <p><strong>getFortify</strong> — For selecting territories and a number of troops to exchange.</p>
+	 * <p>Return country name to move units from, country name to fortify and number of units to move, or "skip" to skip this stage.</p>
+	 * @return String representing fortify command ("from", "to", "units")
+	 */
+	public String getFortify () {
+		return getCommand(Fortify.turn);
+	}
+
+	/* * Internal Methods * */
 	private interface GameMap {
 		Territory getTerritory(int territory);
 		Continent getContinent(int continent);
@@ -180,221 +378,6 @@ public class BeachArcade implements Bot {
 			return this.weight - that.weight;
 		}
 	}
-
-    private BoardAPI board;
-	private PlayerAPI player;
-
-    private final GameMap map;
-
-	protected static int selectedTerritory; // * Holds the ID of the target of an attack
-	private final ArrayList<Continent> continents;
-	private final Turn[] turns;
-	// * initialisation: place your troops inside the cluster, place neutrals away from you
-
-    // * 1. Select main territory (or territories)
-	// * 2. Place troops 3 at a time
-	// * 3. Battle from there, priority on inside continent
-	// * 4. Fortify walls
-
-	// ? When you take over the whole continent, build up walls, then try and take over next continent
-
-	// ? What about a constant value ATTACK_LIMIT, would be a value representing a ratio
-
-    BeachArcade(BoardAPI inBoard, PlayerAPI inPlayer) {
-        board = inBoard;
-        player = inPlayer;
-
-		map = new Map(player.getId());
-
-        map.startTurn(board);
-    }
-
-    /**
-     * <strong>getName</strong> — Gets the name of the bot.
-     *
-     * @return String, Bot's name
-     */
-    public String getName() {
-        return ("SPF 75"); // ? What about throwing debug info in here, so whenever the bot's name is used, we get debug stuff
-        /*
-         *debug will include:
-         *	how many countries in total the bot partialSetMatchs/percentage of the board partialSetMatchled
-         *	how many connecting countries the bot partialSetMatchs
-         *	standard deviation of the troops on each territory
-         *	how many cards are in the bot's hand/whether or not the bot can turn in its cards(to see if it is intentionally not turning them in)
-         * 	how many whole continents the bot owns
-         */
-    }
-
-    private String getCommand(Turn turn) {
-    	map.startTurn(board);
-
-    	return map.stream().filter(turn::canUse).findFirst().map(turn::getCommand).orElse("skip");
-	}
-
-	/**
-	 * <p><strong>getPlacement</strong> — For placing of territories in <em>initialisation stage</em>.</p>
-	 * <p>Chooses a territory from one of the neutrals to place reinforcements on sending it to the board reinforcement (from the neutral player's reserves) on the selected territory.</p>
-	 *
-	 * @param forPlayer The index of the player whose territories we are choosing from.
-	 * @return String, the name of the territory
-	 */
-	public String getPlacement(int forPlayer) {
-		System.out.println("RE-IMPLEMENT THIS");
-		return getRandomName();
-	}
-
-	/* Utility Methods */
-	public static String getRandomName() {
-		return GameData.COUNTRY_NAMES[(int) (Math.random() * GameData.NUM_COUNTRIES)].replaceAll("\\s", "");
-	}
-
-    /**
-     * <p><strong>getReinforcement</strong> — For <em>reinforcing</em> a territory with reserves.</p>
-     * <p>Gets the name of a territory (that belongs to the bot) to place a number of reinforcements (that is valid) onto.</p>
-     *
-     * @return String, command in the form "Territory Name" "Number of Reinforcements".
-     */
-    public String getReinforcement() {
-		return getCommand(Reinforcement.turn);
-    }
-
-    /**
-     * <p><strong>getCardExchange</strong> — Gets cards from the bot's hand to exchange for reserves.</p>
-     * <p>Returns a valid card exchange command; 3 cards to exchange (just first letter, ie. wia), or "<em>skip</em>" if less than 5 cards.</p>
-     *
-     * @return String, command using letters to represent the cards
-     */
-    public String getCardExchange() { // First Iteration Complete
-        // Later it might be possible to strategise what hands are optimal, and if you have 2 different ways to turn in cards, find the best one
-        int[][] validSets = new int[60][3];
-        int[] set = new int[Deck.SET_SIZE];
-        int r = 0;
-        PriorityQueue<Decision> decisions = new PriorityQueue<>();
-
-        for (int i = 0; (i < Deck.NUM_SETS); i++) {
-            System.arraycopy(Deck.SETS[i], 0, set, 0, Deck.SET_SIZE);
-            int count = 0;
-            ArrayList<Card> copyCards = new ArrayList<Card>(player.getCards()); //Takes the cards in the bot's hand and makes a copy to find possible matches with
-            boolean partialSetMatch; // PartialSetMatch is made to cut the loop when a match is found
-            for (int j : set) {
-                partialSetMatch = false;
-                for (int m = 0; (m < copyCards.size()) && !partialSetMatch; m++) {
-                    if (j == copyCards.get(m).getInsigniaId()) {
-                        copyCards.remove(m);
-                        count++;
-                        partialSetMatch = true;
-                    }
-                }
-                if (count == Deck.SET_SIZE) {
-                    System.arraycopy(set, 0, validSets[r], 0, set.length);
-                    r++;
-                    count = 0;
-                }
-            }
-        }
-        for (int i = 0; i < r; i++) { // Takes the int values of the insignias and turns them into usable answers for the bot
-            StringBuilder x = new StringBuilder();
-            System.out.println("-----------------------");
-            for (int j = 0; j < Deck.SET_SIZE; j++) {
-                switch (validSets[i][j]) {
-                    case 0:
-                        x.append("i");
-                        break;
-                    case 1:
-                        x.append("c");
-                        break;
-                    case 2:
-                        x.append("a");
-                        break;
-                    case 3:
-                        x.append("w");
-                        break;
-                    default:
-                    	System.out.println("Error you messed up");
-                }
-            }
-            int weight = 0;
-            for (int j = 0; j < Deck.SET_SIZE; j++) {
-                if (validSets[i][j] == 0 || validSets[i][j] == 1 || validSets[i][j] == 2)// Weights a set so that the wild cards are saved for later use  if possible
-                    weight += 2;
-                else
-                    weight += 1;
-
-            }
-            String possibleDecision = x.toString();
-            decisions.add(new Decision(weight, possibleDecision));
-        }
-        if (decisions.isEmpty()) {
-        	throw new IllegalStateException("No valid set of cards");
-		}
-		return decisions.poll().command;
-    }
-
-    private static class Decision implements Comparable<Decision> {
-        String command;
-        int weight;
-        public Decision(int weight, String command) {
-            this.command = command;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(Decision that) {
-            return this.weight - that.weight;
-        }
-    }
-
-    /**
-     * <p><strong>getBattle</strong> — For getting an attack command.</p>
-     * <p>Used in bot's <em>attack stage</em>. Returns a String containing the country to attack from, the country to attack and the number of units to use, or "skip" to skip attack phase.</p>
-     *
-     * @return String representing attack command ("from", "to", "units")
-     */
-    public String getBattle() {
-    	return getCommand(Battle.turn);
-    }
-
-    /**
-     * <p><strong>getDefence</strong> — For getting a defend command.</p>
-     * <p>Used when other player is in the <em>attack stage</em>. Returns the number of units to defend a territory with.</p>
-     *
-     * @param countryId int, ID of territory to defend.
-     * @return String, number of units
-     */
-    public String getDefence(int countryId) { // Always defend with the most units you can to give the best chance of winning
-        String command = "";
-        if (board.getNumUnits(countryId) >= 2)
-            command += 2;
-        else
-            command += 1;
-        return (command);
-    }
-
-    /**
-     * <p><strong>getMoveIn</strong> — For sending troops after winning battle.</p>
-     * <p>Used when you have won a battle and claimed a territory. Sends a number of troops to the newly conquered territory, leaving both territories with at least one troop. </p>
-     *
-     * @param attackCountryId int, ID of your attacking territory
-     * @return String, number of units
-     */
-    public String getMoveIn(int attackCountryId) {
-//        return getCommand(MoveIn.turn);
-		// > Logic surrounding the value in `selectedTerritory` and the value in attackCountryId
-		Territory attacking = map.getTerritory(attackCountryId);
-		return "";
-    }
-
-	/**
-	 * <p><strong>getFortify</strong> — For selecting territories and a number of troops to exchange.</p>
-	 * <p>Return country name to move units from, country name to fortify and number of units to move, or "skip" to skip this stage.</p>
-	 * @return String representing fortify command ("from", "to", "units")
-	 */
-	public String getFortify () {
-		return getCommand(Fortify.turn);
-	}
-
-
-	/* * Nested Turn * */
 	private static abstract class Turn {
 		protected double ratio;
 
